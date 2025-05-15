5387d93962383319aecbd5293d1267bf1e7b6a5d
blue-hexagon <w0j8uhv5csio@opayq.net>
2025-05-15 14:08:14 +0200

    Features and refinement.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 sudo chmod u+rwx vsftpd_installer.sh
 sudo chmod u+rwx hms_installer.sh
 export COMPANY_NAME="nhi"
-#
+export DEPARTMENTS=("research" "hr" "it" )
+export HOST_IP="192.168.10.37"
--- vsftpd_installer.sh
+++ vsftpd_installer.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 ######################################################################################
-#------------------------------------ Input Data -------------------------------------
+#------------------------------------ Input Data ------------------------------------#
 ######################################################################################
 if [ -z "$COMPANY_NAME" ]; then
-  export COMPANY_NAME="nhi"
+  COMPANY_NAME="nhi"
 fi
-ftp_users=("${COMPANY_NAME}_ro" "${COMPANY_NAME}_rw" "${COMPANY_NAME}_admin")
-ftp_passwords=("Kode1234!" "Kode1234!" "Kode1234!")
+if [ -z "$COMPANY_DOMAIN" ]; then
+  COMPANY_DOMAIN="nhi.it"
+fi
+if [ -z "$HOST_IP" ]; then
+  HOST_IP="192.168.10.37"
+fi
+if [[ ${#DEPARTMENTS[@]} -eq 0 ]]; then
+  DEPARTMENTS=("research" "it")
+fi
+ftp_users=()
+ftp_passwords=()
+
+for dept in "${DEPARTMENTS[@]}"; do
+  ftp_users+=("${COMPANY_NAME}_${dept}_ro")
+  ftp_users+=("${COMPANY_NAME}_${dept}_rw")
+  ftp_passwords+=("Kode1234!" "Kode1234!")
+done
+ftp_users+=("${COMPANY_NAME}_admin")
+ftp_passwords+=("Admin1234!")
+ftp_users+=("${COMPANY_NAME}_ansat")
+ftp_passwords+=("Kode1234!")
 echo "[1/9]: Config data loaded."
 
 ######################################################################################
-#------------------------------------ Initialize ------------------------------------
+#------------------------------------ Initialize -----------------------------------#
 ######################################################################################
 sudo apt update && sudo apt upgrade -y
-sudo apt install nginx vsftpd openssl ufw -y
+sudo apt install nginx-full vsftpd openssl ufw apache2-utils -y
+set -e
 sudo useradd -m "ftpuser"
 systemctl enable --now vsftpd
 systemctl enable --now ufw
@@ -21,7 +41,7 @@ systemctl enable --now nginx
 echo "[2/9]: System initialized."
 
 ######################################################################################
-#---------------------------------------- UFW ----------------------------------------
+#---------------------------------------- UFW ---------------------------------------#
 ######################################################################################
 ufw allow 20/tcp
 ufw allow 21/tcp
@@ -29,41 +49,48 @@ ufw allow 22/tcp
 ufw allow 30000:31000/tcp
 ufw allow 80/tcp
 ufw allow 443/tcp
-ufw allow 900/tcp
 ufw --force enable
 ufw reload
 echo "[3/9]: UFW Configured and activated."
 
 ######################################################################################
-#---------------------------------Create FTP Users-----------------------------------
+#---------------------------------Create FTP Users----------------------------------#
 ######################################################################################
 sudo groupadd sharedftp
-sudo groupadd sharedftp
+usermod -aG sharedftp www-data
 mkdir -p /etc/vsftpd/user_config
 touch /etc/vsftpd/user_list
 for i in "${!ftp_users[@]}"; do
     user="${ftp_users[$i]}"
     pass="${ftp_passwords[$i]}"
-    id "${user}" &>/dev/null || sudo useradd -m "$user" -s /usr/sbin/nologin 
+    id "${user}" &>/dev/null || sudo useradd -m "$user" -s /bin/bash 
     sudo usermod -aG sharedftp "$user"
     echo "$user:$pass" | sudo chpasswd
-    grep -q "^${user}$" /etc/vsftpd/user_list || echo "${user}" >> /etc/vsftpd/user_list
+    if [[ "$user" == *ro ]] || [[ "$user" == *ansat ]] || [[ "$user" == *admin ]]; then
+        htpasswd -cb /etc/nginx/.htpasswd $user $pass
+    elif [[ "$user" == *rw ]] || [[ "$user" == *admin ]]; then
+        grep -q "^${user}$" /etc/vsftpd/user_list || echo "${user}" >> /etc/vsftpd/user_list
+    fi
 done
 echo "[4/9]: Users configured."
 
 ######################################################################################
-#--------------------------------- Configure VSFTPD ----------------------------------
+#--------------------------------- Configure VSFTPD ---------------------------------#
 ######################################################################################
 sudo mkdir -p /srv/ftp/nhi
 sudo chown root:sharedftp /srv/ftp/nhi
-sudo chmod 777 /srv/ftp/nhi
-
-sudo mkdir -p /srv/ftp/nhi/research_papers
-sudo chown root:sharedftp /srv/ftp/nhi/research_papers
-sudo chmod 777 /srv/ftp/nhi/research_papers
+sudo chmod 2775 /srv/ftp/nhi
+for dept in "${DEPARTMENTS[@]}"; do
+    sudo mkdir -p /srv/ftp/nhi/${dept}
+    sudo chown root:sharedftp /srv/ftp/nhi/${dept}
+    sudo chmod 2775 /srv/ftp/nhi/${dept}
+done
+sudo mkdir -p /srv/ftp/software
+sudo chown root:sharedftp /srv/ftp/software
+sudo chmod 2775 /srv/ftp/software
 
 cat <<EOF | sudo tee /etc/vsftpd.conf > /dev/null
-ftpd_banner=Velokmmen til NHIs Sikre FTP Service
+ftpd_banner=Velkommen Til ${COMPANY_NAME^^}'s Sikre FTP Service!
 
 xferlog_enable=YES
 xferlog_file=/var/log/vsftpd.log
@@ -76,7 +103,7 @@ write_enable=YES
 dirlist_enable=YES
 anonymous_enable=NO
 user_config_dir=/etc/vsftpd/user_config
-chown_upload_mode=0777
+chown_upload_mode=0775
 
 chroot_local_user=YES
 chroot_list_enable=YES
@@ -89,7 +116,7 @@ local_umask=002
 
 pasv_min_port=30000
 pasv_max_port=31000
-local_max_rate=100000000
+local_max_rate=1000000000
 
 userlist_file=/etc/vsftpd/user_list
 userlist_deny=NO
@@ -108,10 +135,12 @@ listen=YES
 listen_ipv6=NO
 EOF
 echo "${COMPANY_NAME}_admin" > /etc/vsftpd/vsftpd.chroot
+sed -i '/^Subsystem/s/^/#/' /etc/ssh/sshd_config
 echo "[5/9]: VSFTPD Configured."
-
+### TODO: Lav vsftpd config for hver bruger, i hver department.
+### TODO: Tjek efter useradd flow, om brugerne bliver tilføjet korrekt til de rigtige gruppe.
 ######################################################################################
-#---------------------------- Configure SSL for VSFTPD -------------------------------
+#---------------------------- Configure SSL for VSFTPD ------------------------------#
 ######################################################################################
 if [ ! -f /etc/vsftpd/vsftpd.pem ]; then
 	sudo openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/vsftpd.pem -out /etc/vsftpd/vsftpd.pem
@@ -120,7 +149,7 @@ systemctl restart vsftpd
 echo "[6/9]: SSL Configured for VSFTPD."
 
 ######################################################################################
-#------------------Configure logging and logrotation for VSFTPD-----------------------
+#------------------Configure logging and logrotation for VSFTPD----------------------#
 ######################################################################################
 touch /var/log/vsftpd.log
 chown ftpuser:adm /var/log/vsftpd.log
@@ -135,39 +164,94 @@ sudo tee /etc/logrotate.d/vsftpd > /dev/null <<EOF
 }
 EOF
 echo "[7/9]: Logrotation Configured for VSFTPD."
+######################################################################################
+#-------------------------------- Nginx FTP SSL  ------------------------------------#
+######################################################################################
+#TODO: Certifikatet skal eksporteres til DC CA
+touch /etc/ssl/openssl-ip.cnf
+sudo chown root:root /etc/ssl/openssl-ip.cnf
+sudo chmod 644 /etc/ssl/openssl-ip.cnf
+cat <<EOF | sudo tee /etc/ssl/openssl-ip.cnf > /dev/null
+[ req ]
+default_bits       = 2048
+distinguished_name = req_distinguished_name
+x509_extensions    = v3_req
+prompt             = no
+
+[ req_distinguished_name ]
+CN = $HOST_IP
+
+[ v3_req ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+IP.1 = $HOST_IP
+DNS.1 = ftp.${COMPANY_DOMAIN}
+EOF
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-ip.key -out /etc/ssl/certs/nginx-ip.crt -config /etc/ssl/openssl-ip.cnf
 
 ######################################################################################
-#---------------------------- Configure NGINX FTP Site -------------------------------
+#---------------------------- Configure NGINX FTP Site ------------------------------#
 ######################################################################################
 cat <<EOF | sudo tee /etc/nginx/sites-available/archive > /dev/null
 server {
     listen 80;
-    server_name nhi-archive.local;
+    server_name ftp.${COMPANY_DOMAIN};
+    return 301 https://\$host\$request_uri;
+}
+server {
+    listen 443 ssl;
+    server_name $HOST_IP ftp.${COMPANY_DOMAIN};
 
+    ssl_certificate     /etc/ssl/certs/nginx-ip.crt;
+    ssl_certificate_key /etc/ssl/private/nginx-ip.key;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers HIGH:!aNULL:!MD5;
+    ssl_prefer_server_ciphers on;
     access_log /var/log/nginx/archive_access.log;
+EOF
 
-    location /nhi/ {
-        root /srv/ftp;
+for dept in "${DEPARTMENTS[@]}"; do
+cat <<EOF | sudo tee -a /etc/nginx/sites-available/archive > /dev/null
+    location /$dept/ {
+        alias /srv/ftp/nhi/${dept}/;
         autoindex on;
         autoindex_exact_size off;
         autoindex_localtime on;
         charset utf-8;
         disable_symlinks if_not_owner;
+        auth_basic "${COMPANY_NAME^^}";
+        auth_basic_user_file /etc/nginx/.htpasswd;
     }
-
+EOF
+done
+cat <<EOF | sudo tee -a /etc/nginx/sites-available/archive > /dev/null
     location ~ /\. {
         deny all;
+    }
+    
+    location /software/ {
+        alias /srv/ftp/software/;
+        autoindex on;
+        autoindex_exact_size off;
+        autoindex_localtime on;
+        charset utf-8;
+        disable_symlinks if_not_owner;
+        auth_basic "${COMPANY_NAME^^}";
+        auth_basic_user_file /etc/nginx/.htpasswd;
     }
 }
 EOF
 sudo ln -sf /etc/nginx/sites-available/archive /etc/nginx/sites-enabled/archive
+sudo rm /etc/nginx/sites-enabled/default
 sudo chown -R root:sharedftp /srv/ftp/nhi
 sudo chmod 2775 /srv/ftp/nhi
-nginx -t && systemctl reload nginx
-echo "[8/9]: NGINX Configured (access from <host>/nhi/)."
+sudo sed -i 's/user .*/user ftpuser;/' /etc/nginx/nginx.conf
+echo "[8/9]: NGINX Configured."
 
 ######################################################################################
-#---------------- Configure Cronjob for sanitizing user uploads ----------------------
+#---------- Configure Cronjob for sanitizing user upload permissions ----------------#
 ######################################################################################
 sudo tee /usr/local/bin/fix-ftp-perms.sh > /dev/null <<'EOF'
 #!/bin/bash
@@ -180,13 +264,17 @@ sudo chmod +x /usr/local/bin/fix-ftp-perms.sh
 echo "[9/9]: Cronjob for FTP user submission file sanitation configured."
 
 ######################################################################################
-#------------------------------ Reload all Services ----------------------------------
+#------------------------------ Reload all Services ---------------------------------#
 ######################################################################################
 systemctl restart vsftpd || systemctl reload vsftpd
 systemctl restart ufw 
 systemctl restart nginx
 echo "[DONE]: Reloaded all services"
+echo "[DONE]: Reloading SSH to make sftp blocking take effect."
+sudo systemctl reload ssh
 
 
-usermod -aG sharedftp www-data
-#
+######################################################################################
+#----------------------------------- Fail2Ban ----------------------------------------
+######################################################################################
+apt update && apt install fail2ban
