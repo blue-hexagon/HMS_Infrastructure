769ad6bee67e90d2bdc42f3d62c5cf728d74cf95
blue-hexagon <w0j8uhv5csio@opayq.net>
2025-05-26 11:55:48 +0200

    Fix bracket issue.

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
 # apt install git
 # git clone https://github.com/blue-hexagon/HMS_Infrastructure
+# cd HMS_Infrastructure
+# chmod a+x init.sh
+
 sudo chmod ugo+rwx ./ftpes/installer.sh
 sudo chmod ugo+rwx ./hms_and_lb/installer.sh
 
-if [ -z "$1" ]; then
+if [[ -z "$1" ]]; then
     echo "Usage: $0 {lb|hms|ftpes|sethostname}"
     exit 1
 fi
@@ -13,12 +17,14 @@ export COMPANY_DOMAIN="${COMPANY_NAME}.${COMPANY_TLD}.local"
 export DEPARTMENTS_STR="labs,research,ledelse,hr,it,sikkerhed"
 export HOST_IP=$(ip -4 route get 1.1.1.1 | awk '{print $7}')
 
-if [ "$1" == "lb" ]; then
+if [[ "$1" == "lb" ]]; then
     sudo ./hms_and_lb/installer.sh lb
-elif [ "$1" == "hms" ]; then
+elif [[ "$1" == "hms" ]]; then
     sudo ./hms_and_lb/installer.sh hms
-elif [ "$1" == "ftpes" ]; then
+elif [[ "$1" == "ftpes" ]]; then
     sudo ./ftpes/installer.sh
-elif [ "$1" == "sethostname" ]; then
+elif [[ "$1" == "sethostname" ]]; then
     sudo hostnamectl set-hostname "${HOST_IP//./-}.${COMPANY_DOMAIN}.local"
+else
+    echo "Invalid argument: $1"
 fi
